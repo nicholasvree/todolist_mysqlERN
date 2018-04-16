a682d93744916217a8b1a1122e0cf9b0a325b670
Nicholas Vree <nicholasvree@Nicholass-MacBook-Air.local>
2018-04-15 19:18:58 -0500

    set up express session to capture code

--- uncorkdlist/routes/controller.js
+++ uncorkdlist/routes/controller.js
@@ -1,6 +1,27 @@
 
+
 var db = require("../models");
 
+
+function generateUnid(
+    a                  // placeholder
+  ){
+    return a           // if the placeholder was passed, return
+      ? (              // a random number from 0 to 15
+        a ^            // unless b is 8,
+        Math.random()  // in which case
+        * 16           // a random number from
+        >> a/4         // 8 to 11
+        ).toString(16) // in hexadecimal
+      : (              // or otherwise a concatenated string:
+        [1e10] + 
+        1e10 +
+        1e9
+        ).replace(     // replacing
+          /[01]/g,     // zeroes and ones with
+          generateUnid // random hex digits
+        ).toLowerCase()
+  }
 
 
 
@@ -9,20 +30,23 @@ module.exports = function(app) {
 
       app.get("/api", function(req, res) {
 
-        // console.log(req.query)
+        if(req.query.code == undefined){
+            req.session.currentCode=generateUnid()
+        }else{
+            req.session.currentCode = req.query.code            
+        }
 
+        console.log(req.session.currentCode)
 
-        //   var sessData = req.session;
-        //   sessData.myCode = "TEST";
-        //   res.json(sessData.code);
-
-        req.session.currentCode = req.query.code
-        // console.log(req.session.code) 
-
-        // console.log(sessData.myCode)
-        res.json(req.session.currentCode)
+        res.redirect("/")
         
       });
+
+      //DELETE WHEN READY - route to test session permanence
+      app.get("/api/testSessionValue", function(req, res) {
+              console.log("FROM TEST", req.session.currentCode)
+              res.send(req.session.currentCode)  
+              });
 
 
 }
